d8ca40b25f5bea3575455f58573670a893525490
mochuting <chutingmo@163.com>
2020-03-31 19:36:17 +0800

    first commit

---
+++ src/main/java/cn/archforce/zhifou/ZhifouApplication.java
@@ -0,0 +1,13 @@
+package cn.archforce.zhifou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhifouApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZhifouApplication.class, args);
+    }
+
+}
