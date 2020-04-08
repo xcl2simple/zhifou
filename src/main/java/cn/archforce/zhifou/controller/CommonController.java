961b6915a10df3a9071a6839e4e92c011e557175
mochuting <chutingmo@163.com>
2020-04-08 14:48:07 +0800

    增加上传文件

---
+++ src/main/java/cn/archforce/zhifou/controller/CommonController.java
@@ -0,0 +1,42 @@
+package cn.archforce.zhifou.controller;
+
+import cn.archforce.zhifou.common.JsonResult;
+import cn.archforce.zhifou.common.ResultCodeEnum;
+import cn.archforce.zhifou.service.ICommonService;
+import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+/**
+ * Description:
+ *
+ * @author mochuting
+ * @version 1.0
+ * @date 2020/4/8 14:11
+ * @since JDK 1.8
+ */
+@RestController
+@RequestMapping("/common")
+@Api(description = "公共接口")
+public class CommonController {
+
+    @Autowired
+    private ICommonService commonService;
+
+    @PostMapping("/upload")
+    public JsonResult uploadFileToServer(MultipartFile file) {
+        String path = null;
+        try {
+             path = commonService.save(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return path != null ? JsonResult.success(path) : JsonResult.failure(ResultCodeEnum.SEVER_EXCEPTION);
+    }
+
+}
---
+++ src/main/java/cn/archforce/zhifou/service/impl/CommonService.java
@@ -0,0 +1,43 @@
+package cn.archforce.zhifou.service.impl;
+
+import cn.archforce.zhifou.config.MyConfiguration;
+import cn.archforce.zhifou.service.ICommonService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import java.io.File;
+import java.io.IOException;
+import java.util.Calendar;
+
+/**
+ * Description:
+ *
+ * @author mochuting
+ * @version 1.0
+ * @date 2020/4/8 14:14
+ * @since JDK 1.8
+ */
+@Slf4j
+@Service
+public class CommonService implements ICommonService {
+
+    @Autowired
+    private MyConfiguration myConfiguration;
+
+    @Override
+    public String save(MultipartFile file) throws IOException {
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH) + 1;
+        String yearAndMonth = year + (month / 10 > 0 ? "" + month : "0" + month);
+        String filePath = myConfiguration.getUploadRoot() + yearAndMonth + "/" + file.getOriginalFilename();
+        File dest = new File(filePath);
+        if (!dest.exists()) {
+            dest.getParentFile().mkdirs();
+        }
+        file.transferTo(dest);
+        log.info("upload path:{}", filePath);
+        return filePath;
+    }
+}
