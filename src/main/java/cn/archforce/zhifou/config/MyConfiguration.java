c665c99fb8a6f5094ac909697056c779cd174d0d
mochuting <chutingmo@163.com>
2020-04-08 13:52:42 +0800

    富文本保存本地

---
+++ src/main/java/cn/archforce/zhifou/config/MyConfiguration.java
@@ -0,0 +1,53 @@
+package cn.archforce.zhifou.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * Description:
+ *
+ * @author mochuting
+ * @version 1.0
+ * @date 2020/4/8 12:00
+ * @since JDK 1.8
+ */
+@Data
+@Component
+@ConfigurationProperties(prefix = "archforce.zhifou")
+public class MyConfiguration {
+
+    public static final String PATH_UPLOAD = "/upload/";
+    public static final String PATH_TEXT = "/text/";
+    public static final String SUFFIX_TEXT = ".txt";
+
+    public static String UPLOAD_PATH, TEXT_PATH;
+
+    /**
+     * 文件保存根目录
+     */
+    private String root;
+
+    /**
+     * 获取文件上传地址
+     * @return
+     */
+    public String getUploadRoot() {
+        if (UPLOAD_PATH == null) {
+            UPLOAD_PATH = this.root.concat(PATH_UPLOAD).replaceAll("//", "/");
+        }
+        return UPLOAD_PATH;
+    }
+
+    /**
+     * 获取富文本内容存放地址
+     * @return
+     */
+    public String getTextRoot() {
+        if (TEXT_PATH == null) {
+            TEXT_PATH = this.root.concat(PATH_TEXT).replaceAll("//","/");
+        }
+        return TEXT_PATH;
+    }
+
+}
--- src/main/java/cn/archforce/zhifou/service/impl/QuestionServiceImpl.java
+++ src/main/java/cn/archforce/zhifou/service/impl/QuestionServiceImpl.java
@@ -1,12 +1,16 @@
 package cn.archforce.zhifou.service.impl;
 
+import cn.archforce.zhifou.config.MyConfiguration;
 import cn.archforce.zhifou.dao.IQuestionDao;
 import cn.archforce.zhifou.mapper.QuestionMapper;
 import cn.archforce.zhifou.model.entity.Question;
 import cn.archforce.zhifou.service.IQuestionService;
+import cn.archforce.zhifou.utils.TextUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import java.io.IOException;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -26,6 +30,9 @@ public class QuestionServiceImpl implements IQuestionService {
     @Autowired
     private IQuestionDao questionDao;
 
+    @Autowired
+    private MyConfiguration myConfiguration;
+
     @Override
     public List<Question> selectQuestionByIndex(Integer sort, Integer startIndex) {
         return questionMapper.selectAll();
@@ -36,6 +43,24 @@ public class QuestionServiceImpl implements IQuestionService {
         if (question == null || exists(question)) {
             return false;
         }
+
+        String txtPath = null;
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH) + 1;
+        String yearAndMonth = year + (month / 10 > 0 ? "" + month : "0" + month);
+        String fileName = calendar.getTimeInMillis() + myConfiguration.SUFFIX_TEXT;
+        try {
+             txtPath = TextUtil.saveToLocal(question.getContent(), myConfiguration.getTextRoot() + yearAndMonth, fileName);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        question.setContent(txtPath);
+        question.setCreateTime(new Date());
+        question.setAnsweredNum(0);
+        question.setViewedNum(0);
+
         questionDao.add(question);
         return true;
     }
